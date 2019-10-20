964cdef3ff493e90959cf397399f3eeb0c3cf07b
mj946 <mj946@192.168.2.89>
2019-10-19 23:03:00 -0400

    First git commit

---
+++ Lectures/src/CalcMethods.java
@@ -0,0 +1,37 @@
+
+public class CalcMethods {
+
+	
+	
+	public int add(int num1, int num2)
+	{
+		int i=num1;
+		int j= num2;
+		
+		return i+j;
+	}
+	
+	public int division(int num1, int num2)
+	{
+		int i=num1;
+		int j= num2;
+		
+		return i/j;
+	}
+	
+	public int multiply(int num1, int num2)
+	{
+		int i=num1;
+		int j= num2;
+		
+		return i*j;
+	}
+	
+	public int subtract(int num1, int num2)
+	{
+		int i=num1;
+		int j= num2;
+		
+		return i-j;
+	}
+}
---
+++ Lectures/src/Calculator.java
@@ -0,0 +1,21 @@
+
+public class Calculator {
+
+
+	public static void main(String[] args) {
+			
+		CalcMethods a = new CalcMethods();
+		int add= a.add(10, 20);
+		int division= a.division(20, 10);
+		int multiply= a.multiply(20, 10);
+		int subtract= a.subtract(10, 20);
+		
+		System.out.println(division);	
+		
+		String s1= "Selenium";
+		s1="sel";
+		System.out.println(s1);
+
+	}
+
+}
